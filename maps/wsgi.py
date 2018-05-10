5067ea209ad00cfdc15fc46a756c43817bdbfc13
Mohan Dharavath <md42591@corp.cyient.com>
2018-05-10 19:19:20 +0530

    Initial commit

--- maps/wsgi.py
+++ maps/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maps.settings")
+
+# application = get_wsgi_application()
+
+
 import os
+import sys
 
+path = '/home/mohan0925/pythonpoc'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pythonpoc.settings'
+#
+## then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maps.settings")
-
 application = get_wsgi_application()
